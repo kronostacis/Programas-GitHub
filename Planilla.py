d741a610823839047cac5c5fadf3efc6edc643a8
kronostacis <63477527+kronostacis@users.noreply.github.com>
2021-08-17 21:14:51 -0400

    Practica

---
+++ Planilla.py
@@ -0,0 +1,14 @@
+!/usr/bin/python
+ -*- coding: utf-8 -*-
+
+# import <biblioteca>
+# from <biblioteca> import <modulo>
+# import <biblioteca> as <alias>
+
+#Declaración de funciones
+
+#Código principal
+if __name__ == '__main__':
+     #Instrucciones
+
+# No escribir codigo en este nivel
